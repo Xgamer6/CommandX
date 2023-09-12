677d59433a65870661f39fcb3bac64fe69215eff
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-09-12 15:31:42 +0200

    Fixed Plugin
    Updated from Api 4.0.0 to 5.0.0
    Added Startup Message

---
+++ src/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TN_Studios\CommandXV2;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase { 
+
+    protected function onEnable() : void
+    {  
+        $this->getLogger()->info("💎Plugin enabled💎");
+    }
+
+}
