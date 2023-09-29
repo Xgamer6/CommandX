ab51d6196120960c5307827f1025c93f1da4b183
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-09-29 16:52:12 +0200

    Added Function to the command plugininfo

--- src/Main.php
+++ src/Main.php
@@ -1,14 +1,24 @@
+```php
 <?php
 
 namespace TN_Studios\CommandXV2;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase { 
+class Main extends PluginBase {
 
-    protected function onEnable() : void
-    {  
-        $this->getLogger()->info("💎Plugin enabled💎");
+    public function onEnable() {
+        // Plugin startup logic
     }
 
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "plugindev") { // Change "testcommand" to the desired command name
+            $sender->sendMessage("This is a test message."); // Customize the message here
+            return true;
+        }
+        return false;
+    }
 }
+```
