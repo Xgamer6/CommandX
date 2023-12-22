de50abfc61c34282a991cdf987aa39b4eb8217f0
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 16:24:14 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,29 +1,26 @@
 <?php
 
-namespace TN_Studios\CommandXV2;
+namespace Xgamer6\CommandX;
 
+use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
-  public function onEnable(){
-    $this->getLogger()->info("CommandX enabled");
-  }
-  
-  public function onDisable(){
-    $this->getLogger()->info("CommandX disabled");
-  }
-  
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-    if($command->getName() === "plugininfo"){
-      if($sender instanceof Player){
-        $sender->sendMessage("Dies ist das CommandXV2 Plugin von TN Studios");
-      }else{
-        $sender->sendMessage("Only for Players");
-      }
-      return true;
+class MeinPlugin extends PluginBase {
+
+    public function onEnable() {
+        $this->getLogger()->info("MeinPlugin wurde aktiviert!");
     }
-    return false;
-  }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "test") {
+            $sender->sendMessage("hall9");
+            return true;
+        }
+        return false;
+    }
+
+    public function onDisable() {
+        $this->getLogger()->info("MeinPlugin wurde deaktiviert!");
+    }
 }
