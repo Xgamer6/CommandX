dc10f8e9a204fe2d9bf397aced331d29b2523145
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 17:11:29 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -9,26 +9,22 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
-    public function onEnable(): void {
+    public function onEnable() {
         $this->getLogger()->info("§aPlugin enabled");
         $this->saveDefaultConfig();
         $this->reloadConfig();
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if ($command->getName() === "cmd1") {
+        if ($command->getName() === "test") {
             $message = $this->getConfig()->get("message");
             $sender->sendMessage($message);
-            return true;
-        } elseif ($command->getName() === "cmdx" && isset($args[0]) && $args[0] === "reload") {
-            $this->reloadConfig();
-            $sender->sendMessage("§aConfig.yml just reloaded");
             return true;
         }
         return false;
     }
 
-    public function onDisable(): void {
+    public function onDisable() {
         $this->getLogger()->info("§cPlugin disabled");
     }
 }
