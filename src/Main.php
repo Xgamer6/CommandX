f5a5d83e0063ca4fa35e7a02050834203f3105cf
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 16:46:20 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,22 +5,26 @@ namespace TN_Studios\CommandX;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
-    public function onEnable(): void {
-        $this->getLogger()->info("CommandX enabled");
+    public function onEnable() {
+        $this->getLogger()->info("§aPlugin enabled");
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "test") {
-            $sender->sendMessage("Test");
+            $message = $this->getConfig()->get("message");
+            $sender->sendMessage($message);
             return true;
         }
         return false;
     }
 
-    public function onDisable(): void {
-        $this->getLogger()->info("CommandX disabled");
+    public function onDisable() {
+        $this->getLogger()->info("§cPlugin disabled");
     }
 }
