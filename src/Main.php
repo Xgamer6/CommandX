4411a0b4c9e571aaa21322609e190a5a26b1011d
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 18:45:52 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -3,37 +3,14 @@
 namespace TN_Studios\CommandX;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
-    public function onEnable(): void {
+    public function onEnable() {
         $this->getLogger()->info("§aPlugin enabled");
-        $this->saveDefaultConfig();
-        $this->reloadConfig();
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if ($command->getName() === "test") {
-            $message = $this->getConfig()->get("message");
-            $sender->sendMessage($message);
-            return true;
-        } elseif ($command->getName() === "reload" && $sender->hasPermission("commandx.reload")) {
-            $this->reloadPlugin();
-            $sender->sendMessage("§aCommandX reloaded");
-            return true;
-        }
-        return false;
-    }
-
-    public function onDisable(): void {
+    public function onDisable() {
         $this->getLogger()->info("§cPlugin disabled");
-    }
-    
-    private function reloadPlugin() {
-        $this->getServer()->getPluginManager()->disablePlugin($this);
-        $this->getServer()->getPluginManager()->enablePlugin($this);
     }
 }
