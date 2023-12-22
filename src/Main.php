3aa59e63fdf7dcb013aaf72cef8ea874d869e526
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 19:01:39 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -3,14 +3,38 @@
 namespace TN_Studios\CommandX;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
 
     public function onEnable(): void {
         $this->getLogger()->info("§aPlugin enabled");
+
+        $this->saveResource("join.yml", false);
+        $this->saveResource("leave.yml", false);
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->getConfig()->set("joinMessage", $this->getConfig()->get("joinMessage", "Willkommen, {player}!"));
+        $this->getConfig()->set("leaveMessage", $this->getConfig()->get("leaveMessage", "{player} hat den Server verlassen."));
     }
 
     public function onDisable(): void {
         $this->getLogger()->info("§cPlugin disabled");
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $joinMessage = str_replace("{player}", $player->getName(), $this->getConfig()->get("joinMessage"));
+        $event->setJoinMessage($joinMessage);
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $leaveMessage = str_replace("{player}", $player->getName(), $this->getConfig()->get("leaveMessage"));
+        $event->setQuitMessage($leaveMessage);
     }
 }
