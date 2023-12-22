606738eee549c400ddd96ebf72bda8ee61c7d21d
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-22 16:27:25 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Xgamer6\CommandX;
+namespace TN_Studios\CommandX;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
@@ -9,18 +9,18 @@ use pocketmine\command\CommandSender;
 class MeinPlugin extends PluginBase {
 
     public function onEnable() {
-        $this->getLogger()->info("MeinPlugin wurde aktiviert!");
+        $this->getLogger()->info("CommandX enabled");
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "test") {
-            $sender->sendMessage("hall9");
+            $sender->sendMessage("Test");
             return true;
         }
         return false;
     }
 
     public function onDisable() {
-        $this->getLogger()->info("MeinPlugin wurde deaktiviert!");
+        $this->getLogger()->info("CommandX disabled");
     }
 }
