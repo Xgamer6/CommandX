a3826ff8d0ed6935f5635ed4ccdd1aa5a56878da
Xgamer6 <142106680+Xgamer6@users.noreply.github.com>
2023-12-23 08:44:06 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -6,66 +6,45 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\utils\Config;
-use pocketmine\form\Form;
-use pocketmine\form\SimpleForm;
-use pocketmine\form\element\Input;
 
 class Main extends PluginBase implements Listener {
 
     /** @var Config */
-    private $config;
+    private $joinConfig;
+    /** @var Config */
+    private $leaveConfig;
 
     public function onEnable(): void {
-        $this->getLogger()->info("§aPlugin enabled!");
+        $this->getLogger()->info("§aPlugin enabled");
 
-        $this->saveResource("config.yml", false);
+        $this->saveResource("join.yml", false);
+        $this->saveResource("leave.yml", false);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->joinConfig = new Config($this->getDataFolder() . "join.yml", Config::YAML);
+        $this->leaveConfig = new Config($this->getDataFolder() . "leave.yml", Config::YAML);
     }
 
     public function onDisable(): void {
-        $this->getLogger()->info("§cPlugin disabled!");
+        $this->getLogger()->info("§cPlugin disabled");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if ($command->getName() === "cmdx-form") {
-            if ($sender instanceof Player) {
-                $title = $this->config->get("title", "Standardtitel");
-                $description = $this->config->get("description", "Standardbeschreibung");
-                $buttonText = $this->config->get("first_button_text", "Standard-Button");
-                $buttonCommand = $this->config->get("first_button_command", "/standard-command");
-
-                $this->openCustomForm($sender, $title, $description, $buttonText, $buttonCommand);
-                return true;
-            } else {
-                $sender->sendMessage("Dieser Befehl kann nur von einem Spieler verwendet werden.");
-                return false;
-            }
-        }
-        return false;
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $joinMessage = $this->joinConfig->get("joinMessage", "Willkommen, {player}!");
+        $joinMessage = str_replace("{player}", $player->getName(), $joinMessage);
+        $event->setJoinMessage($joinMessage);
     }
 
-    private function openCustomForm(Player $player, string $title, string $description, string $buttonText, string $buttonCommand) {
-        $form = new SimpleForm(function (Player $player, $data) use ($buttonCommand) {
-            if ($data === null) {
-                return;
-            }
-            
-            // Hier kannst du den ausgewählten Button weiterverarbeiten
-            if ($data === 0) {
-                // Der erste Button wurde ausgewählt, führe den Befehl aus
-                $player->chat($buttonCommand);
-            }
-        });
-
-        $form->setTitle($title);
-        $form->setContent($description);
-        $form->addButton($buttonText);
-
-        $player->sendForm($form);
+    public function onPlayerQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $leaveMessage = $this->leaveConfig->get("leaveMessage", "{player} hat den Server verlassen.");
+        $leaveMessage = str_replace("{player}", $player->getName(), $leaveMessage);
+        $event->setQuitMessage($leaveMessage);
     }
 }
